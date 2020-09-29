6b020ef4e0751e198f5d5fb3d81406290775e10e
Jake Meyers <jakejake92@yahoo.com>
2020-09-29 15:05:06 -0600

    refactor

--- src/components/ItemsList.js
+++ src/components/ItemsList.js
@@ -1,20 +1,21 @@
 import React from "react";
 
 const ItemsList = (props) => {
-    
-    const todos = props.todos.map((todo) => {
-        return <div>{todo}</div>
-    });
+	const todos = props.todos.map((todo) => {
+		return (
+			<div className='list-item'>
+				<p>{todo}</p>
+				<i class='check icon'></i>
+			</div>
+		);
+	});
 
-
-    
-    return (
-        <div>
-            {todos}
-        </div>
-    );
-      
-    
-}
+	return (
+		<div>
+			{todos}
+			<i class='check icon'></i>
+		</div>
+	);
+};
 
 export default ItemsList;
