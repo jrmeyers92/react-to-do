cedbce4d7c4b31eb6436ac6c578269f6a06379dc
Jake Meyers <jakejake92@yahoo.com>
2020-09-29 15:26:09 -0600

    basic functionality working

--- src/App.js
+++ src/App.js
@@ -3,29 +3,22 @@ import HeadingMain from "./components/HeadingMain";
 import Input from "./components/Input";
 import ItemsList from "./components/ItemsList";
 
-
 class App extends React.Component {
-    state = {todo: ["",""] };
+	state = { todo: [] };
 
-    onFormSubmit = (term) => {
-        this.setState({todo : [...this.state.todo, term]});
-    };
+	onFormSubmit = (term) => {
+		this.setState({ todo: [...this.state.todo, term] });
+	};
 
-
-
-    render() {
-    
-
-        return (
-            <div className="ui container">
-                <HeadingMain />
-                <Input onSubmit = {this.onFormSubmit} />
-                <ItemsList todos = {this.state.todo} />
-            </div>
-          
-            
-        );
-    }
+	render() {
+		return (
+			<div className='ui container'>
+				<HeadingMain />
+				<Input onSubmit={this.onFormSubmit} />
+				<ItemsList todos={this.state.todo} />
+			</div>
+		);
+	}
 }
 
 export default App;
--- src/components/ItemsList.js
+++ src/components/ItemsList.js
@@ -1,21 +1,24 @@
 import React from "react";
+import "./ItemList.css";
 
 const ItemsList = (props) => {
+	const onXClick = () => {
+		console.log("hi");
+	};
+
 	const todos = props.todos.map((todo) => {
 		return (
 			<div className='list-item'>
-				<p>{todo}</p>
+				<div className='left'>
+					<i class='x icon x-icon' onClick={onXClick}></i>
+					<p className='list-item__item'>{todo}</p>
+				</div>
 				<i class='check icon'></i>
 			</div>
 		);
 	});
 
-	return (
-		<div>
-			{todos}
-			<i class='check icon'></i>
-		</div>
-	);
+	return <div className='theItemList'>{todos}</div>;
 };
 
 export default ItemsList;
